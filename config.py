c39b106c39c8dba8d9113225bdcba27b920823cb
Ivan Zerocoderov <zerocodemethodist@gmail.com>
2025-01-23 23:28:05 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+iam_token = "ваш iam токен"
+catalog_id = "ваш id каталога"
+url_1 = "https://llm.api.cloud.yandex.net/foundationModels/v1/imageGenerationAsync"
+url_2 = "https://llm.api.cloud.yandex.net:443/operations"
